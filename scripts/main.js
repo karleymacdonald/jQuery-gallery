6faaa8032042555e47a3eb8b7ad2ab8151318878
Karley MacDonald <karleymacdonald@gmail.com>
2018-11-08 16:18:30 -0500

    Updating with the working logic

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    console.log('ready');
+	$('ul img').on('click', function() {
+	    const src = $(this).attr('data-main');
+	    console.log(src);
+	    $('img#main').attr('src', src);
+	});
+});
